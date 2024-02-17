03f047d8d354643e2ae5c8e646627407603cede1
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-02-17 14:43:41 +0900

    2차원 벡터 생성

---
+++ gamemath-3-1/Source/Runtime/Math/Private/Sphere.cpp
@@ -0,0 +1,28 @@
+
+#include "Precompiled.h"
+using namespace CK;
+
+Sphere::Sphere(const std::vector<Vector3>& InVertices)
+{
+	size_t cnt = InVertices.size();
+	if (cnt == 0)
+	{
+		return;
+	}
+
+	Vector3 sum;
+	for (const auto& v : InVertices)
+	{
+		sum += v;
+	}
+
+	Center = sum / (float)cnt;
+	float distanceSquared = 0.f;
+	Center = sum / (float)cnt;
+	Vector3 farthestPoint = (*std::max_element(InVertices.begin(), InVertices.end(),
+		[&](Vector3 const& lhs, Vector3 const& rhs)
+		{
+			return (Center - lhs).SizeSquared() < (Center - rhs).SizeSquared();
+		}));
+	Radius = (farthestPoint - Center).Size();
+}
