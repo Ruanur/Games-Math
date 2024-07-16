50a780b8208d3f9da3169e3722e5f2d7b2cffb8c
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-07-16 20:54:44 +0900

    삼각 함수로 하트 그리기

---
+++ gamemath-4-1/Source/Player/SoftRenderer2D.cpp
@@ -0,0 +1,130 @@
+
+#include "Precompiled.h"
+#include "SoftRenderer.h"
+#include <random>
+using namespace CK::DD;
+
+// 격자를 그리는 함수
+void SoftRenderer::DrawGizmo2D()
+{
+	auto& r = GetRenderer();
+	const auto& g = Get2DGameEngine();
+
+	// 그리드 색상
+	LinearColor gridColor(LinearColor(0.8f, 0.8f, 0.8f, 0.3f));
+
+	// 뷰의 영역 계산
+	Vector2 viewPos = g.GetMainCamera().GetTransform().GetPosition();
+	Vector2 extent = Vector2(_ScreenSize.X * 0.5f, _ScreenSize.Y * 0.5f);
+
+	// 좌측 하단에서부터 격자 그리기
+	int xGridCount = _ScreenSize.X / _Grid2DUnit;
+	int yGridCount = _ScreenSize.Y / _Grid2DUnit;
+
+	// 그리드가 시작되는 좌하단 좌표 값 계산
+	Vector2 minPos = viewPos - extent;
+	Vector2 minGridPos = Vector2(ceilf(minPos.X / (float)_Grid2DUnit), ceilf(minPos.Y / (float)_Grid2DUnit)) * (float)_Grid2DUnit;
+	ScreenPoint gridBottomLeft = ScreenPoint::ToScreenCoordinate(_ScreenSize, minGridPos - viewPos);
+
+	for (int ix = 0; ix < xGridCount; ++ix)
+	{
+		r.DrawFullVerticalLine(gridBottomLeft.X + ix * _Grid2DUnit, gridColor);
+	}
+
+	for (int iy = 0; iy < yGridCount; ++iy)
+	{
+		r.DrawFullHorizontalLine(gridBottomLeft.Y - iy * _Grid2DUnit, gridColor);
+	}
+
+	ScreenPoint worldOrigin = ScreenPoint::ToScreenCoordinate(_ScreenSize, -viewPos);
+	r.DrawFullHorizontalLine(worldOrigin.Y, LinearColor::Red);
+	r.DrawFullVerticalLine(worldOrigin.X, LinearColor::Green);
+}
+
+// 게임 오브젝트 목록
+
+
+// 최초 씬 로딩을 담당하는 함수
+void SoftRenderer::LoadScene2D()
+{
+	// 최초 씬 로딩에서 사용하는 모듈 내 주요 레퍼런스
+	auto& g = Get2DGameEngine();
+
+}
+
+// 게임 로직과 렌더링 로직이 공유하는 변수
+Vector2 currentPosition;
+float currentScale = 10.f;
+
+// 게임 로직을 담당하는 함수
+void SoftRenderer::Update2D(float InDeltaSeconds)
+{
+	// 게임 로직에서 사용하는 모듈 내 주요 레퍼런스
+	auto& g = Get2DGameEngine();
+	const InputManager& input = g.GetInputManager();
+
+	// 게임 로직의 로컬 변수
+	static float moveSpeed = 100.f;
+	static float scaleMin = 5.f;
+	static float scaleMax = 20.f;
+	static float scaleSpeed = 20.f;
+
+	Vector2 inputVector = Vector2(input.GetAxis(InputAxis::XAxis), input.GetAxis(InputAxis::YAxis)).GetNormalize();
+	Vector2 deltaPosition = inputVector * moveSpeed * InDeltaSeconds;
+	float deltaScale = input.GetAxis(InputAxis::ZAxis) * scaleSpeed * InDeltaSeconds;
+
+	// 물체의 최종 상태 설정
+	currentPosition += deltaPosition;
+	currentScale = Math::Clamp(currentScale + deltaScale, scaleMin, scaleMax);
+}
+
+// 렌더링 로직을 담당하는 함수
+void SoftRenderer::Render2D()
+{
+	// 렌더링 로직에서 사용하는 모듈 내 주요 레퍼런스
+	auto& r = GetRenderer();
+	const auto& g = Get2DGameEngine();
+
+	// 배경에 격자 그리기
+	DrawGizmo2D();
+
+	// 렌더링 로직의 로컬 변수
+	float rad = 0.f;
+	static float increment = 0.001f;
+	static std::vector<Vector2> hearts;
+
+	// 하트를 구성하는 점 생성
+	if (hearts.empty())
+	{
+		for (rad = 0.f; rad < Math::TwoPI; rad += increment)
+		{
+			float sin = sinf(rad);
+			float cos = cosf(rad);
+			float cos2 = cosf(2 * rad);
+			float cos3 = cosf(3 * rad);
+			float cos4 = cosf(4 * rad);
+			float x = 16.f * sin * sin * sin;
+			float y = 13 * cos - 5 * cos2 - 2 * cos3 - cos4;
+			hearts.push_back(Vector2(x, y));
+		}
+	}
+
+	for (auto const& v : hearts)
+	{
+		r.DrawPoint(v * currentScale + currentPosition, LinearColor::Blue);
+	}
+
+	// 현재 위치와 스케일을 화면에 출력
+	r.PushStatisticText(std::string("Position : ") + currentPosition.ToString());
+	r.PushStatisticText(std::string("Scale : ") + std::to_string(currentScale));
+}
+
+// 메시를 그리는 함수
+void SoftRenderer::DrawMesh2D(const class DD::Mesh& InMesh, const Matrix3x3& InMatrix, const LinearColor& InColor)
+{
+}
+
+// 삼각형을 그리는 함수
+void SoftRenderer::DrawTriangle2D(std::vector<DD::Vertex2D>& InVertices, const LinearColor& InColor, FillMode InFillMode)
+{
+}
